c1f2c433d2a3f81e44c82f7a9cfbf2672dcfc35c
Team Onestone <team.onestone@gmail.com>
2019-05-25 22:25:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo "REMOVE PREVIOUS BUILD"
-rm -rf build
+#echo "REMOVE PREVIOUS BUILD"
+#rm -rf build
 
-echo "BUILDING"
+#echo "BUILDING"
 #mkdir build
 #g++ -Wall -o build/main src/main.cpp
 
 #cmake .
 #make 
 
-RC=$?
+#RC=$?
 
-if [ $RC -ne 0 ]; then
-  echo "BUILD FAILURE"
-else
-  echo "BUILD SUCCESS"
-fi
+#if [ $RC -ne 0 ]; then
+#  echo "BUILD FAILURE"
+#else
+#  echo "BUILD SUCCESS"
+#fi
