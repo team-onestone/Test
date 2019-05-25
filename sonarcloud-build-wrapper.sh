14ed2ea853abc2aeaa5edc6d50a3c77cf8aef60f
Team Onestone <team.onestone@gmail.com>
2019-05-25 19:00:10 +0200

    Create sonarcloud-build-wrapper.sh

---
+++ sonarcloud-build-wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "RUNNING SONARCLOUD BUILD WRAPPER"
+
+mkdir build-wrapper
+cd build-wrapper
+wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip build-wrapper-linux-x86.zip
+cd ..
+
+./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-outputs ./build.sh
