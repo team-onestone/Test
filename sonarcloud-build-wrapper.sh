1fa133f6fbf84c9290be663087051beec942d96b
Team Onestone <team.onestone@gmail.com>
2019-05-25 20:47:23 +0200

    Update sonarcloud-build-wrapper.sh

--- sonarcloud-build-wrapper.sh
+++ sonarcloud-build-wrapper.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 echo "UPDATING THE TRAVIS RUNNER"
+apt update && apt upgrade -y
+
+echo "INSTALLING SONAR CUBE SANNER & WRAPPER"
+mkdir sonarqube && cd sonarqube
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
+wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+
+unzip sonar-scanner-cli-3.3.0.1492-linux.zip
+unzip build-wrapper-linux-x86.zip
+export PATH=$PATH:/home/travis/team-onestone/Test/sonarqube/sonar-scanner-3.3.0.1492-linux/bin
+export PATH=$PATH:/home/travis/team-onestone/Test/sonarqube/unzip build-wrapper-linux-x86
+cd ..
+
+echo "RUNNING SONARCLOUD BUILD WRAPPER"
+build-wrapper --out-dir bw-outputs ./build.sh
+
+echo "RUNNING SONARCLOUD SCANNER"
+sonar-scanner \
+-Dsonar.login=$SONAR_TOKEN
